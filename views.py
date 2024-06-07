f2678fc28a919e6b0480e0ed703ee3c32adc9397
waimerle <167164782+waimerle@users.noreply.github.com>
2024-06-07 18:10:01 +0200

    Update views.py

--- views.py
+++ views.py
@@ -9,11 +9,8 @@ def Homeseite(request):
 
 	Rezept_Filename = "/var/www/django-projekt/LeckerMeister/Rezepte.json"
 
-	if not os.path.isfile(Rezept_Filename):
-		return HttpResponse("Die Rezeptdatei existiert nicht!")
-
 	with open(Rezept_Filename, "r") as file: 
-		Rezept_list = json.load(file)
+		Rezept_list = json.loads(file.read())
 
 	rezepte = []
 	for rezept in Rezept_list:
@@ -23,15 +20,55 @@ def Homeseite(request):
 			"Zutaten": rezept.get("Zutaten", ""),
 			"Zubereitung": rezept.get("Zubereitung", ""),
 			"Zubereitungszeit": rezept.get("Zubereitungszeit", ""),
+			"Kategorie": rezept.get("Kategorie", ""),
 		})
 
+
 	return render(request, "LeckerMeister/Homeseite.html", {"rezepte": rezepte})
+
 
 def Suchseite(request):
 	return render(request, "LeckerMeister/Suchseite.html")
 
 def Upload(request):
-        return render(request, "LeckerMeister/Upload.html")
+
+    if request.method == 'POST':
+        # Formulardaten abrufen
+        rezept_name = request.POST.get('recipeName')
+        rezept_bild = request.FILES.get('recipeImage')
+        zutaten = request.POST.get('ingredients')
+        zubereitung = request.POST.get('instructions')
+        kategorie = request.POST.get('category')
+
+        neues_rezept = {
+            "Rezeptbild": rezept_bild.name,  # Bildname speichern, tatsÃ¤chliche Handhabung erforderlich
+            "name": rezept_name,
+            "Zutaten": zutaten,
+            "Zubereitung": zubereitung,
+            "Kategorie": kategorie
+        }
+
+        rezept_filename = "/var/www/django-projekt/LeckerMeister/Rezepte.json"
+        try:
+            with open(rezept_filename, 'r') as file:
+                rezepte = json.load(file)
+        except FileNotFoundError:
+            rezepte = []
+
+
+        rezepte.append(neues_rezept)
+
+        with open(rezept_filename, 'w') as file:
+            json.dump(rezepte, file, indent=4)
+
+
+        with open(f'/var/www/django-projekt/LeckerMeister/uploads/{rezept_bild.name}', 'wb+') as destination:
+            for chunk in rezept_bild.chunks():
+                destination.write(chunk)
+
+        return HttpResponse("Rezept erfolgreich hochgeladen")
+
+    return render(request, 'LeckerMeister/Upload.html')
 
 def Kochbuch(request):
 	return render(request, "LeckerMeister/Kochbuch.html")
