d93009019e99f9e60137e8157e8baa3a6fa978a1
waimerle <167164782+waimerle@users.noreply.github.com>
2024-06-12 15:44:13 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,15 +2,14 @@ from django.urls import path
 from LeckerMeister import views as app_views
 
 urlpatterns = [
-	path("", app_views.Anmeldung, name='anmeldung'),
+	path("Anmeldung.html", app_views.Anmeldung, name='Anmeldung'),
+	path("Registrierung.html", app_views.Registrierung, name='Registrierung'),
 	path("Homeseite.html", app_views.Homeseite, name='Homeseite'),
 	path("Suchseite.html", app_views.Suchseite),
 	path("Upload.html", app_views.Upload, name='Upload'),
 	path("Kochbuch.html", app_views.Kochbuch),
-	path("Profil.html", app_views.Profil),
+	path("Profil.html", app_views.Profil, name='Profil'),
 	path("Datenschutz.html", app_views.Datenschutz),
 	path("Impressum.html", app_views.Impressum),
 	path("AGB.html", app_views.AGB),
-
 ]
-
