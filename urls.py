5895fe8d67ab99b40571a471e115e8f8bd0d2279
Sophiie18 <167548382+Sophiie18@users.noreply.github.com>
2024-06-17 13:53:47 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from LeckerMeister import views as app_views
+from . import views
 
 urlpatterns = [
-        path("", app_views.Allgemein),
-	
-]
+	path("Anmeldung.html", app_views.Anmeldung, name='Anmeldung'),
+	path("Abmeldung/", app_views.Abmeldung, name='Abmeldung'),
+	path("Registrierung.html", app_views.Registrierung, name='registrierung'),
+	path("Homeseite.html", app_views.Homeseite, name='Homeseite'),
+	path("Suchseite.html", app_views.Suchseite, name='Suchseite'),
+	path("Upload.html", app_views.Upload, name='Upload'),
+	path("Kochbuch.html", app_views.Kochbuch, name='Kochbuch'),
+	path("Profil.html", app_views.Profil, name='Profil'),
+	path("AGB.html", app_views.AGB),
+	path("Datenschutz.html", app_views.Datenschutz),
+	path("Impressum.html", app_views.Impressum),
+	path('add_comment/<str:rezept_name>/', views.add_comment, name='add_comment'),
 
+]
